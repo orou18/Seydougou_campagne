811b28bdc591e05a6c82f1bd5defcffe73ee1471
NadegeBocoAssogba <nadegeassogba338@gmail.com>
2025-12-11 16:19:46 +0100

    Update Vision and Mission section text
    J'ai changé le titre de la section Vision et mission, ajouté de texte pour remplacer le Lorem ipsum et commenter le code de la signature.

--- src/components/shared/VisionMissionSection.tsx
+++ src/components/shared/VisionMissionSection.tsx
@@ -24,19 +24,22 @@ const VisionMissionSection: React.FC = () => {
           <div className="lg:w-1/2 w-full lg:pl-16">
             {/* Étiquette d'accentuation (Orange Ivoirien) */}
             <span className="inline-block px-3 py-1 text-xs font-semibold uppercase tracking-wider text-white bg-[#F4E8A5] rounded-full mb-4">
-              Vision & Mission
+              Vision & priorités
             </span>
 
             {/* Titre */}
             <h2 className="text-4xl font-extrabold text-gray-900 leading-snug mb-4">
-              Chaque Personne est Importante pour l'Élection dans Notre Pays
+              Notre vision pour la Côte d'Ivoire
             </h2>
 
             {/* Texte de description */}
             <p className="text-lg text-gray-600 mb-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing, sed eiusmod
-              tempor sit amet elit dolor sit amet elit. Nous travaillons à
-              assurer une participation démocratique juste et éclairée.
+              𝘊𝘩𝘦𝘳𝘴 𝘱𝘢𝘳𝘦𝘯𝘵𝘴, 𝘯𝘰𝘵𝘳𝘦 𝘤𝘢𝘯𝘥𝘪𝘥𝘢𝘵𝘶𝘳𝘦 𝘷𝘪𝘴𝘦 𝘦́𝘨𝘢𝘭𝘦𝘮𝘦𝘯𝘵 𝘢̀ 𝘳𝘦𝘯𝘧𝘰𝘳𝘤𝘦𝘳 𝘭’𝘶𝘯𝘪𝘰𝘯 
+              𝘦𝘵 𝘭𝘢 𝘧𝘳𝘢𝘵𝘦𝘳𝘯𝘪𝘵𝘦́ 𝘦𝘯𝘵𝘳𝘦 𝘎𝘣𝘦́𝘭𝘦́𝘣𝘢𝘯 𝘦𝘵 𝘚𝘦𝘺𝘥𝘰𝘶𝘨𝘰𝘶, 
+              𝘮𝘪𝘴𝘦𝘴 𝘢̀ 𝘮𝘢𝘭 𝘥𝘶𝘳𝘢𝘯𝘵 𝘤𝘦𝘴 𝘲𝘶𝘪𝘯𝘻𝘦 (𝟣𝟧) 𝘢𝘯𝘯𝘦́𝘦𝘴 𝘱𝘢𝘳 𝘭𝘢 𝘱𝘳𝘪𝘰𝘳𝘪𝘴𝘢𝘵𝘪𝘰𝘯 
+              𝘥’𝘪𝘯𝘵𝘦́𝘳𝘦̂𝘵𝘴 𝘱𝘦𝘳𝘴𝘰𝘯𝘯𝘦𝘭𝘴 𝘢𝘶 𝘥𝘦́𝘵𝘳𝘪𝘮𝘦𝘯𝘵 𝘥𝘶 𝘣𝘪𝘦𝘯-𝘦̂𝘵𝘳𝘦 𝘥𝘦 𝘯𝘰𝘴 𝘱𝘰𝘱𝘶𝘭𝘢𝘵𝘪𝘰𝘯𝘴. 
+              𝘕𝘰𝘶𝘴 𝘯𝘦 𝘴𝘰𝘮𝘮𝘦𝘴 𝘤𝘰𝘯𝘵𝘳𝘦 𝘱𝘦𝘳𝘴𝘰𝘯𝘯𝘦 ; 𝘯𝘰𝘶𝘴 𝘴𝘰𝘮𝘮𝘦𝘴 𝘤𝘰𝘯𝘵𝘳𝘦 𝘶𝘯 𝘴𝘺𝘴𝘵𝘦̀𝘮𝘦 𝘲𝘶𝘪, 
+              𝘥𝘶𝘳𝘢𝘯𝘵 𝘵𝘰𝘶𝘵 𝘤𝘦 𝘵𝘦𝘮𝘱𝘴, 𝘢 𝘵𝘳𝘢𝘩𝘪 𝘴𝘦𝘴 𝘦𝘯𝘨𝘢𝘨𝘦𝘮𝘦𝘯𝘵𝘴 𝘦𝘵 𝘮𝘢𝘯𝘲𝘶𝘦́ 𝘢̀ 𝘭’𝘢𝘱𝘱𝘦𝘭 𝘥𝘶 𝘱𝘦𝘶𝘱𝘭𝘦.
             </p>
 
             {/* Bouton d'action (Vert Ivoirien) */}
@@ -46,9 +49,9 @@ const VisionMissionSection: React.FC = () => {
               </button>
 
               {/* Signature simulée */}
-              <div className="text-gray-500 italic text-xl font-serif">
+              {/* <div className="text-gray-500 italic text-xl font-serif">
                 Signature du Leader
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
